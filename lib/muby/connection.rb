0814cf8c729c60efe77b59aa6113a947eff4abbf
Will Pall <pall.will@gmail.com>
2023-09-03 19:08:02 -0400

    Implement enable/disable echo for when the user is entering their password
    This was a real pain. There aren't many libraries about this out there, and RFCs 854 and 857
    specificy what should happen between the server/client communication, they don't talk about
    which specific bytes to use or exactly which ACK the receiver should use.
    
    I ended up cobbling together the correct byte sequences from multiple sources, but Ruby
    didn't _really_ want to deal with those either. Finally, while I know the enable/disable
    bytes are correct, the ACKs are all over the place, so we may have to add more of those
    to the list in the future.

--- lib/muby/connection.rb
+++ lib/muby/connection.rb
@@ -5,6 +5,29 @@ class Muby::Connection < EM::Connection
   attr_reader :username
   attr_accessor :user
 
+  module PASSWORD_STATES
+    INVALID = :invalid
+    BLANK = :blank
+    VALID = :valid
+  end
+
+  module LOGIN_STATES 
+    USERNAME = :username
+    PASSWORD = :password
+    LOGGED_IN = :logged_in
+  end
+
+  module USERNAME_STATES
+    BLANK = :blank
+    EXISTING = :existing
+    NEW = :new
+  end
+
+  module ECHO_STATUS
+    ON = :on
+    OFF = :off
+  end
+
   #
   # EventMachine handlers
   #
@@ -12,8 +35,13 @@ class Muby::Connection < EM::Connection
   def post_init
     @username = nil
 
+    @current_password_state = PASSWORD_STATES::INVALID
+    @current_login_state = LOGIN_STATES::USERNAME
+    @current_username_state = USERNAME_STATES::BLANK
+    @current_echo_status = ECHO_STATUS::ON
+
     puts 'A client has connected...'
-    ask_for_username
+    send_line('Enter your username:')
   end
 
   def unbind
@@ -23,87 +51,100 @@ class Muby::Connection < EM::Connection
     puts "[info] #{@username} has left" if entered_username?
   end
 
+  ##
+  # This is everything about the connection. We can send data whenever we
+  # want, but the process is always going to end up "blocking" here until we
+  # hear back from the user.
   def receive_data(data)
-    data = data.strip
+    @current_response = data.strip
 
-    if logged_in?
-      command, arguments = data.split(' ', 2)
-
-      if command.present?
-        command = command.downcase.to_sym
-
-        if Muby::CommandHandler.is_command?(command)
-          Muby::CommandHandler.dispatch_command(self, command, arguments)
-        elsif self.user.can_move?(command)
-          self.user.move_to(command)
-        else
-          self.send_line('Command `' + command.to_s + '` not found. Type `help` for a list of commands.')
-        end
-      else
-        self.send_line(self.user.prompt)
-      end
-    elsif !entered_username?
-      verify_username(data)
-    else
-      verify_password(data)
-    end
-  end
-
-
-  #
-  # Username handling
-  #
-
-  def entered_username?
-    @username.present?
-  end
-
-  def logged_in?
-    self.user.present?
-  end
-
-  def ask_for_username
-    send_line('Enter your username:')
-  end
-
-  def verify_username(input)
-    if input.empty?
-      send_line('Blank usernames are not allowed. Try again.')
-      ask_for_username
-    else
-      @username = input
-      existing_user = Muby::User.where(username: @username).first
-
-      if existing_user.blank?
-        send_line('We haven\'t seen you before. Choose a password:')
-      else
-        send_line("Welcome back #{@username}! Enter your password:")
-      end
-    end
-  end
-
-  # TODO: Turn off echo for passwords?
-  def verify_password(input)
-    existing_user = Muby::User.where(username: @username).first
-
-    if input.blank?
-      send_line('Please enter a valid password:')
+    # skip responding if it's just the client sending us an ACK
+    if is_echo_response?(@current_response)
       return
-    elsif existing_user.blank?
-      self.user = Muby::User.create(
-        username: @username,
-        name: @username,
-        password: input
+    end
+
+    # if they're not currently logged in, as in "playing", then we've gotta go
+    # through sign in process
+    if !logged_in?
+      if @current_login_state == LOGIN_STATES::USERNAME
+        do_username_handling
+      elsif @current_login_state == LOGIN_STATES::PASSWORD
+        do_password_handling
+      else
+        # something bad happened
+        puts 'Auth error'
+      end
+    else
+      # we're got a logged in user that needs to deal with a command, so let's send it off to the handler
+      Muby::CommandHandler.handle_command(self, data)
+    end
+  end
+
+  ####
+  # Login handling
+  #
+  # NOTE: we _must_ add a carriage return anywhere echo would be off or the
+  # client doesn't know what to do with cursor placement
+  ####
+
+  def do_username_handling
+    if @current_response.blank?
+      send_line("Blank usernames are not allowed. Try again:\r")
+      return
+    end
+
+    # next try to find a user
+    @current_user = Muby::User.find_by_username(@current_response)
+
+    if @current_user.present?
+      send_line("Welcome back, #{@current_user.username}! Enter your password:\r")
+      @current_username_state = USERNAME_STATES::EXISTING
+    end
+
+    if @current_user.blank?
+      send_line("We haven't seen you before, #{@current_response}. Choose a password:\r")
+      @current_username_state = USERNAME_STATES::NEW
+      @current_user = Muby::User.new(
+        username: @current_response,
+        name: @current_response
       )
-    elsif existing_user.password != input
-      send_line('That password is incorrect. Try again:')
-      return
-    else
-      self.user = existing_user
-      self.user.save
     end
 
+    @current_login_state = LOGIN_STATES::PASSWORD
+    disable_echo
+  end
+
+  def do_password_handling
+    if @current_response.blank?
+      send_line("Please enter a valid password:\r")
+      @current_password_state = PASSWORD_STATES::BLANK
+      return
+    end
+
+    if @current_username_state == USERNAME_STATES::NEW
+      @current_user.password = @current_response
+      @current_user.save
+    end
+
+    if @current_username_state == USERNAME_STATES::EXISTING
+      if @current_user.password != @current_response
+        send_line("That password is incorrect. Try again:\r")
+        @current_password_state = PASSWORD_STATES::INVALID
+        return
+      end
+    end
+
+    @current_login_state = LOGIN_STATES::LOGGED_IN
+    enable_echo
+
+    # should go ahead an do this here, otherwise we have to wait for a response
+    # before we can even start showing anything else
+    do_login_finalization
+  end
+
+  def do_login_finalization
     @@connected_clients.push(self)
+    self.user = @current_user
     self.user.connection = self
 
     # ensure they're in a valid room
@@ -119,6 +160,37 @@ class Muby::Connection < EM::Connection
 
     send_to_clients(Muby::MessageHelper.info_message("#{@username} has joined the game"), Muby::ConnectionHelper.other_peers(self))
     puts Paint[@username, :green] + ' has joined'
+  end
+
+  #
+  # Username handling
+  #
+
+  def entered_username?
+    @username.present?
+  end
+
+  def logged_in?
+    self.user.present?
+  end
+
+  def enable_echo
+    @current_echo_status = ECHO_STATUS::ON
+    send_data("\xFF\xFC\x01".b)
+  end
+
+  def disable_echo
+    @current_echo_status = ECHO_STATUS::OFF
+    send_data("\xFF\xFB\x01".b)
+  end
+
+  def is_echo_response?(response)
+    echo_response_codes = [
+      "\xFF\xFD\x01".b,
+      "\xFF\xFE\x01".b
+    ]
+
+    echo_response_codes.include?(response)
   end
 
   #
