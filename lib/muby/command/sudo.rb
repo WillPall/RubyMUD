d63fae777123c4bdc5e757313761eb6e79877714
Will Pall <pall.will@gmail.com>
2023-09-03 19:08:01 -0400

    Add initial admin command

---
+++ lib/muby/command/sudo.rb
@@ -0,0 +1,29 @@
+class Muby::Command::Sudo < Muby::Command
+  def initialize
+    self.name = :sudo
+    self.description = 'Execute admin commands (only available to admins)'
+  end
+
+  def execute(client, arguments)
+    # TODO: make this role-based
+    if client.user.username == 'will'
+      command, arguments = arguments.split(' ', 2)
+      if command.present?
+        command = command.downcase.to_sym
+
+        case command
+        when :room_id
+          client.send_line(client.user.room.id)
+        when :users
+          client.send_line(client.user.room.users.pluck(:username).join(', '))
+        end
+      else
+        client.send_line('Do not recognize that `sudo` command')
+      end
+    else
+      client.send_line('You cannot do that')
+    end
+  end
+end
+
+Muby::CommandHandler.register_command(Muby::Command::Sudo.new)
