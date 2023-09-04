85b92aeaedea5412aacd545be939402f039a16cb
Will Pall <pall.will@gmail.com>
2023-09-03 23:40:20 -0400

    Make `sudo` command based on superuser rather than hardcoded and remove unneccessary guard

--- lib/command/sudo.rb
+++ lib/command/sudo.rb
@@ -6,27 +6,24 @@ class Command::Sudo < Command
     requires_full_name!
 
     self.name = :sudo
-    self.description = 'Execute admin commands (only available to admins)'
+    self.description = 'Execute admin commands (room_id, users)'
   end
 
   def execute(client, arguments)
-    # TODO: make this role-based
-    if client.user.username == 'will'
-      command, arguments = arguments.split(' ', 2)
-      if command.present?
-        command = command.downcase.to_sym
+    command, arguments = arguments.split(' ', 2)
 
-        case command
-        when :room_id
-          client.send_line(client.user.room.id)
-        when :users
-          client.send_line(client.user.room.online_users.pluck(:username).join(', '))
-        end
-      else
-        client.send_line('Do not recognize that `sudo` command')
+    if command.present?
+      command = command.downcase.to_sym
+
+      # TODO: will make these real commands when we have some that are more useful
+      case command
+      when :room_id
+        client.send_line(client.user.room.id)
+      when :users
+        client.send_line(client.user.room.online_users.pluck(:username).join('\n\t'))
       end
     else
-      client.send_line('You cannot do that')
+      client.send_line('Do not recognize that `sudo` command')
     end
   end
 end
